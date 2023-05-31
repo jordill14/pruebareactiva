607ec93dba408f65d6f0435302b1de5add67289e
jordiwow <jordi.llonch@wowshop.com>
2023-05-31 20:56:19 +0200

    new changes

---
+++ src/main/java/com/backend/pruebareactiva/PruebareactivaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pruebareactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebareactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebareactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/pruebareactiva/PruebareactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pruebareactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebareactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
